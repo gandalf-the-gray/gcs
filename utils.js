18bfdfc08ada52100327b825dba43f9d4616269a
Nitesh Yadav <nitesh9501@hotmail.com>
2023-05-20 05:27:00 +0530

    fix

--- utils.js
+++ utils.js
@@ -5,7 +5,7 @@ async function log(message, category = "error") {
     try {
         const date = new Date().toLocaleString("in-EN", { timeZone: "Asia/Kolkata"});;
         const logMessage = `${category} ${date} => ${message}\n`;
-        await fs.appendFile(path.join(process.cwd(), "logs.js"), logMessage);
+        await fs.appendFile(require.resolve(path.join(process.cwd(), "logs.txt")), logMessage);
         return true;
     } catch(e) {
         return false;
@@ -13,12 +13,12 @@ async function log(message, category = "error") {
 }
 
 async function getWazowski() {
-    const filePath = path.join(process.cwd(), "img", "wazowski.jpg");
+    const filePath = require.resolve(path.join(process.cwd(), "assets", "images", "wazowski.jpg"));
     return await fs.readFile(filePath);
 }
 
 async function getLogFile() {
-    const filePath = path.join(process.cwd(), "logs.js");
+    const filePath = require.resolve(path.join(process.cwd(), "logs.txt"))
     return await fs.readFile(filePath, {encoding: "utf-8"});
 }
 
