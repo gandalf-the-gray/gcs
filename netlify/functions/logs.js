e7f84238117e99115528b8a54d5ff715423b1e0a
Nitesh Yadav <nitesh9501@hotmail.com>
2023-05-20 04:58:13 +0530

    fix

--- netlify/functions/logs.js
+++ netlify/functions/logs.js
@@ -3,8 +3,8 @@ const { getLogFile } = require("../../utils.js");
 exports.handler = async function(event, __) {
     return {
         statusCode: 200,
-        headers: {'Content-type' : 'text/plain'},
-        body: await getLogFile(),
-        isBase64Encoded : true,
+        // headers: {'Content-type' : 'text/plain'},
+        body: JSON.stringify({files: await getLogFile()}),
+        // isBase64Encoded : true,
     }
 }
