14fc71ce7918282133c3bc2a270191624b439ff0
Nitesh Yadav <nitesh9501@hotmail.com>
2023-05-20 04:54:28 +0530

    fix

--- utils.js
+++ utils.js
@@ -18,8 +18,12 @@ async function getWazowski() {
 }
 
 async function getLogFile() {
-    const filePath = path.join(process.cwd(), "logs.txt");
-    return await fs.readFile(filePath);
+    // const filePath = path.join(process.cwd(), "logs.txt");
+    return await fs.readFile("logs.txt", {encoding: 'utf-8'});
 }
+
+// (async function() {
+//     console.log(await getLogFile());
+// })();
 
 module.exports = {log, getWazowski, getLogFile};
