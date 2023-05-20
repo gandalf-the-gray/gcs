8cb6678899a07f532aa685a4262e5713f89d9140
Nitesh Yadav <nitesh9501@hotmail.com>
2023-05-20 15:08:05 +0530

    fix

--- netlify/functions/logs.js
+++ netlify/functions/logs.js
@@ -2,6 +2,7 @@ const { log, getLogFile} = require("../../utils.js");
 
 exports.handler = async function() {
     try {
+        await log("trying to connect to mongo", "info");
         return {
             statusCode: 200,
             headers: {
