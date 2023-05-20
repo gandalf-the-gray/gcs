1694367e97344fbe194e9df27f46787c70c41011
Nitesh Yadav <nitesh9501@hotmail.com>
2023-05-20 06:46:20 +0530

    fix

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const {log, getWazowski} = require("../../utils.js");
+const {log, getWazowski, verifyQueryBody, verifyAdmin} = require("../../utils.js");
 
 (async function() {
     while(true) {
@@ -29,7 +29,9 @@ const queryModel = mongoose.model("query", querySchema);
 
 exports.handler = async function(event, __) {
     const requestPath = event.path.split("/.netlify/functions/api/")[1];
+    const requestMethod = event.httpMethod;
     const pathSplits = requestPath.split("/");
+
     if(pathSplits.length > 2 || (pathSplits[0] !== "queries" && pathSplits[0] !== "query")) {
         return {
             statusCode: 200,
@@ -38,10 +40,74 @@ exports.handler = async function(event, __) {
             isBase64Encoded : true,
         }
     } else {
-        if(pathSplits[0] === "queries") {
-            
-        } else {
-
+        let statusCode = 200;
+        let body = {};
+        try{
+            if(pathSplits[0] === "queries") {
+                if(requestMethod === "GET") {
+                    body = await queryModel.find({});
+                } else if(requestMethod === "POST") {
+                    const reqBody = JSON.parse(event.body);
+                    const errorMessage = verifyQueryBody(reqBody);
+                    if(errorMessage !== null) {
+                        body = {message: errorMessage};
+                    } else {
+                        statusCode = 201;
+                        body = await queryModel.create({name: reqBody.name, message: reqBody.message, phoneNo: reqBody.phoneNo, email: reqBody.email});
+                    }
+                } else if (requestMethod === "DELETE") {
+                    const errorMessage = verifyAdmin(event);
+                    if(errorMessage !== null) {
+                        statusCode = 401;
+                        body = {message: errorMessage};
+                    } else {
+                        await queryModel.delete({});
+                        statusCode = 204;
+                    }
+                } else {
+                    statusCode = 400;
+                    body = {message: "PUT method not allowed"};
+                }
+            } else {
+                const queryId = pathSplits[1];
+                if(!queryId) {
+                    statusCode = 422;
+                    body = {message: "Query ID is required"};
+                } else {
+                    if(requestMethod === "GET") {
+                        body = await queryModel.find({_id: queryId});
+                    } else if(!await queryModel.exists({_id: queryId})) {
+                        statusCode = 400;
+                        body = {message: "Query doesn't exist"};
+                    } else if(requestMethod === "PUT") {
+                        const reqBody = JSON.parse(event.body);
+                        const errorMessage = verifyQueryBody(reqBody);
+                        if(errorMessage !== null) {
+                            statusCode = 422;
+                            body = {message: errorMessage};
+                        } else {
+                            body = await queryModel.findOneAndUpdate({_id: queryId}, {$set: {name: reqBody.name, message: reqBody.message, phoneNo: reqBody.phoneNo, email: reqBody.email}}, {new: true});
+                        }
+                    } else if(requestMethod === "DELETE") {
+                        const errorMessage = verifyAdmin(event);
+                        if(errorMessage !== null) {
+                            statusCode = 401;
+                            body = {message: errorMessage};
+                        } else {
+                            await queryModel.deleteOne({_id: queryId});
+                        }
+                    } else {
+                        statusCode = 400;
+                        body = {message: "POST method not allowed"};
+                    }
+                }
+            }
+            return {
+                statusCode,
+                body: JSON.stringify(body),
+            }
+        } catch(e) {
+            statusCode = 500;
         }
     }
 }
--- utils.js
+++ utils.js
@@ -12,6 +12,7 @@ async function log(message, category = "error") {
     }
 }
 
+// Content related functions
 async function getWazowski() {
     const filePath = path.join(process.cwd(), "assets", "images", "wazowski.jpg");
     return await fs.readFile(filePath, {encoding: "base64"});
@@ -22,4 +23,21 @@ async function getLogFile() {
     return await fs.readFile(filePath, {encoding: "utf-8"});
 }
 
-module.exports = {log, getWazowski, getLogFile};
+// Validations functions
+function verifyAdmin(event) {
+    return event.headers.authorization && event.headers.authorization === process.env.ADMIN_PASSWORD ? null : false;
+}
+
+function verifyQueryBody(body) {
+    if(body.name && body.phoneNo && body.message) {
+        if(body.phoneNo.length === 10) {
+            return null;
+        } else {
+            return "Phone number should have exactly 10 digits";
+        }
+    } else {
+        return "Name, message and phone number are required";
+    }
+}
+
+module.exports = {log, getWazowski, getLogFile, verifyAdmin, verifyQueryBody};
