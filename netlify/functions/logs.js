fe3e393c66a6548705bdd6a5f6645d844c19bba4
Nitesh Yadav <nitesh9501@hotmail.com>
2023-05-20 05:40:02 +0530

    fix

--- netlify/functions/logs.js
+++ netlify/functions/logs.js
@@ -4,6 +4,6 @@ exports.handler = async function(event, __) {
     return {
         statusCode: 200,
         // headers: {'Content-type' : 'text/plain'},
-        body: JSON.stringify({files: await getFiles(),})
+        body: JSON.stringify({files: (await getFiles()).map((item) => ({name: item.name, isDir: item.isDirectory()})),})
     }
 }
