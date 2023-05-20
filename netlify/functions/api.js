a597affc4c93d1b7fcbed6ed9c1c29c09008b099
Nitesh Yadav <nitesh9501@hotmail.com>
2023-05-20 15:41:01 +0530

    fix

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const {log, getWazowski, verifyQueryBody, verifyAdmin, getLogFile} = require("../../utils.js");
 
 async function connectToMongo() {
-    await log("trying to connect to mongob", "info");
     await mongoose.connect(`mongodb+srv://nitesh:ilSX2zLf1MDNM8pS@cluster0.z2qmspp.mongodb.net/`);
+    await("connected to mongo", "info");
 }
 
 // Query model
@@ -31,68 +31,68 @@ exports.handler = async function(event, __) {
     } else {
         try{
             await connectToMongo();
-            // const queryModel = mongoose.model("query", querySchema);
+            const queryModel = mongoose.model("query", querySchema);
             let statusCode = 200;
             let body = {};
-            // if(pathSplits[0] === "queries") {
-            //     if(requestMethod === "GET") {
-            //         body = await queryModel.find({});
-            //     } else if(requestMethod === "POST") {
-            //         const reqBody = JSON.parse(event.body);
-            //         const errorMessage = verifyQueryBody(reqBody);
-            //         if(errorMessage !== null) {
-            //             body = {message: errorMessage};
-            //         } else {
-            //             statusCode = 201;
-            //             body = await queryModel.create({name: reqBody.name, message: reqBody.message, phoneNo: reqBody.phoneNo, email: reqBody.email});
-            //         }
-            //     } else if (requestMethod === "DELETE") {
-            //         const errorMessage = verifyAdmin(event);
-            //         if(errorMessage !== null) {
-            //             statusCode = 401;
-            //             body = {message: errorMessage};
-            //         } else {
-            //             await queryModel.delete({});
-            //             statusCode = 204;
-            //         }
-            //     } else {
-            //         statusCode = 400;
-            //         body = {message: "PUT method not allowed"};
-            //     }
-            // } else {
-            //     const queryId = pathSplits[1];
-            //     if(!queryId) {
-            //         statusCode = 422;
-            //         body = {message: "Query ID is required"};
-            //     } else {
-            //         if(requestMethod === "GET") {
-            //             body = await queryModel.find({_id: queryId});
-            //         } else if(!await queryModel.exists({_id: queryId})) {
-            //             statusCode = 400;
-            //             body = {message: "Query doesn't exist"};
-            //         } else if(requestMethod === "PUT") {
-            //             const reqBody = JSON.parse(event.body);
-            //             const errorMessage = verifyQueryBody(reqBody);
-            //             if(errorMessage !== null) {
-            //                 statusCode = 422;
-            //                 body = {message: errorMessage};
-            //             } else {
-            //                 body = await queryModel.findOneAndUpdate({_id: queryId}, {$set: {name: reqBody.name, message: reqBody.message, phoneNo: reqBody.phoneNo, email: reqBody.email}}, {new: true});
-            //             }
-            //         } else if(requestMethod === "DELETE") {
-            //             const errorMessage = verifyAdmin(event);
-            //             if(errorMessage !== null) {
-            //                 statusCode = 401;
-            //                 body = {message: errorMessage};
-            //             } else {
-            //                 await queryModel.deleteOne({_id: queryId});
-            //             }
-            //         } else {
-            //             statusCode = 400;
-            //             body = {message: "POST method not allowed"};
-            //         }
-            //     }
-            // }
+            if(pathSplits[0] === "queries") {
+                if(requestMethod === "GET") {
+                    body = await queryModel.find({});
+                } else if(requestMethod === "POST") {
+                    const reqBody = JSON.parse(event.body);
+                    const errorMessage = verifyQueryBody(reqBody);
+                    if(errorMessage !== null) {
+                        body = {message: errorMessage};
+                    } else {
+                        statusCode = 201;
+                        body = await queryModel.create({name: reqBody.name, message: reqBody.message, phoneNo: reqBody.phoneNo, email: reqBody.email});
+                    }
+                } else if (requestMethod === "DELETE") {
+                    const errorMessage = verifyAdmin(event);
+                    if(errorMessage !== null) {
+                        statusCode = 401;
+                        body = {message: errorMessage};
+                    } else {
+                        await queryModel.delete({});
+                        statusCode = 204;
+                    }
+                } else {
+                    statusCode = 400;
+                    body = {message: "PUT method not allowed"};
+                }
+            } else {
+                const queryId = pathSplits[1];
+                if(!queryId) {
+                    statusCode = 422;
+                    body = {message: "Query ID is required"};
+                } else {
+                    if(requestMethod === "GET") {
+                        body = await queryModel.find({_id: queryId});
+                    } else if(!await queryModel.exists({_id: queryId})) {
+                        statusCode = 400;
+                        body = {message: "Query doesn't exist"};
+                    } else if(requestMethod === "PUT") {
+                        const reqBody = JSON.parse(event.body);
+                        const errorMessage = verifyQueryBody(reqBody);
+                        if(errorMessage !== null) {
+                            statusCode = 422;
+                            body = {message: errorMessage};
+                        } else {
+                            body = await queryModel.findOneAndUpdate({_id: queryId}, {$set: {name: reqBody.name, message: reqBody.message, phoneNo: reqBody.phoneNo, email: reqBody.email}}, {new: true});
+                        }
+                    } else if(requestMethod === "DELETE") {
+                        const errorMessage = verifyAdmin(event);
+                        if(errorMessage !== null) {
+                            statusCode = 401;
+                            body = {message: errorMessage};
+                        } else {
+                            await queryModel.deleteOne({_id: queryId});
+                        }
+                    } else {
+                        statusCode = 400;
+                        body = {message: "POST method not allowed"};
+                    }
+                }
+            }
             return {
                 statusCode,
                 body: JSON.stringify(body),
@@ -100,7 +100,7 @@ exports.handler = async function(event, __) {
         } catch(e) {
             return {
                 statusCode: 500,
-                body: JSON.stringify({message: e.message, auth: {username: process.env.MONGO_DB_USERNAME, pass: process.env.MONGO_DB_PASSWORD}})
+                body: JSON.stringify({message: e.message})
             }
         }
     }
